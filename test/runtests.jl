1cc3979c7087ef7deb85f449bf8ed3444acb83bd
Keita Nakamura <keita.nakamura.1109@gmail.com>
2026-05-30 15:56:12 +0900

    Use swap delete for active samples

--- test/runtests.jl
+++ test/runtests.jl
@@ -106,8 +106,8 @@ end
         rng = StableRNG(1234)
         pts = (@inferred PoissonDiskSampling.generate(rng, T, rand(rng), (0,8), (0,10)))::Vector{NTuple{2, T}}
         centroid = collect(reduce(.+, pts) ./ length(pts))
-        T == Float64 && @test centroid ≈ [4.022368487702714, 4.8572828187868256]
-        T == Float32 && @test centroid ≈ [3.9188566f0, 4.8546963f0]
+        T == Float64 && @test centroid ≈ [4.0089555591919455, 4.93973579235562]
+        T == Float32 && @test centroid ≈ [3.980507f0, 4.991337f0]
     end
 
     if Threads.nthreads() > 1
